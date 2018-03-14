cb99bb299aaaaf40da5f2e1481610e9866a1337e
MKLau <matthewklau@fas.harvard.edu>
2018-03-14 18:12:38 -0400

    initializing

---
+++ R/prov.capture.R
@@ -0,0 +1,15 @@
+#' prov.capture --- Capture R language provencance.
+#' OUTPUT = Provenance from code stored in memory.
+#' 
+#' Captures and records provenance from a script provided by the user.
+#' 
+#' @param x A script who's provenance should be recorded.
+#' @return Stores provenance in memory in PROV-JSON format.
+#' @importFrom base trace
+#' @export prov.capture
+#' @author ELizabeth Hu and Matthew K. Lau
+
+prov.capture <- function(x = "script"){
+    #### Insert funcntion tracing activities here
+    
+}
